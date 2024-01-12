80b8801ea53bf1866d631c9657751991d6fad5c2
JuanCompaire <juancompaire2004@gmail.com>
2024-01-12 10:14:04 +0100

    agregado poder enviar mensajes con nombre

--- cliente.py
+++ cliente.py
@@ -3,17 +3,20 @@ import sys
 
 server_ip = "localhost"
 server_port = 9069
+client_name = input("Ingrese su nombre: ")
 
 client_socket = socket(AF_INET,SOCK_STREAM)
 client_socket.connect((server_ip,server_port))
 
+client_socket.send(client_name.encode())
+
 while True:
 
     #write message
-    message = input()
+    message = input("Pon tu mensaje :")
     if message != "exit":
         #send the message
-        client_socket.send(message.encode())
+        client_socket.send(f" {message}".encode())
         #recive the message
         response = client_socket.recv(1024).decode()
         print(response)
--- server.py
+++ server.py
@@ -12,12 +12,17 @@ server_socket.listen()
 while True:
     socket_conection, client_adress = server_socket.accept()
     print("conectado con un cliente. ",client_adress)
+
+    client_name= socket_conection.recv(1024).decode()
+    
     while True:
         message = socket_conection.recv(1024).decode()
-        print(message)
 
         if message == "exit":
             break
+        
+        print(f"{client_name}: {message}")
+
         #send the message to the client
         socket_conection.send(input().encode())
     print("desconectado con un cliente. ",client_adress)
