74099991b693f0ef04a4b5c880e5ba061238ac3e
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2024-03-09 16:04:16 +0100

    fixeado ahora de verdad

--- cliente.py
+++ cliente.py
@@ -1,29 +1,23 @@
 from socket import *
 import sys
 
-
 server_ip = "localhost"
 server_port = 9069
 client_name = input("Ingrese su nombre: ")
 
-client_socket = socket(AF_INET,SOCK_STREAM)
-client_socket.connect((server_ip,server_port))
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((server_ip, server_port))
 
 client_socket.send(client_name.encode())
 
 while True:
-
-    #write message
+    # write message
     message = input("Pon tu mensaje :")
     if message != "exit":
-        #send the message
-        client_socket.send(f" {message}".encode())
-        #recive the message
-        response = client_socket.recv(1024).decode()
-        print("SERVER :"+str(response))
-
+        # send the message
+        client_socket.send(f"{client_name}: {message}".encode())
     else:
         client_socket.send(message.encode())
-        #close conection
+        # close connection
         client_socket.close()
         sys.exit()
--- server.py
+++ server.py
@@ -1,30 +1,34 @@
 from socket import *
+import threading
 
-server_adress = "localhost"
+server_ip = "localhost"
 server_port = 9069
 
-#creating a new socket
-server_socket = socket(AF_INET,SOCK_STREAM)
-#
-server_socket.bind((server_adress,server_port))
-server_socket.listen()
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind((server_ip, server_port))
+server_socket.listen(5)
+print("Server listening on port", server_port)
+
+clients = []
+
+def handle_client(client_socket, client_address):
+    print(f"Accepted connection from {client_address}")
+    client_name = client_socket.recv(1024).decode()
+    print(f"{client_name} has joined the chat")
+
+    while True:
+        message = client_socket.recv(1024).decode()
+        if message == "exit" or not message:
+            print(f"{client_name} has left the chat")
+            clients.remove(client_socket)
+            client_socket.close()
+            break
+        print(f"{client_name}: {message}")
+        for client in clients:
+            if client != client_socket:
+                client.send(message.encode())
 
 while True:
-    socket_conection, client_adress = server_socket.accept()
-    client_name= socket_conection.recv(1024).decode()
-
-    print("conectado con ",client_name,client_adress)
-    
-    while True:
-        message = socket_conection.recv(1024).decode()
-
-        if message == "exit":
-            break
-        
-        print(f"{client_name}: {message}")
-
-        #send the message to the client
-        socket_conection.send(input().encode())
-    print("desconectado con un cliente. ",client_adress)
-    #close conection
-    socket_conection.close()
+    client_socket, client_address = server_socket.accept()
+    clients.append(client_socket)
+    threading.Thread(target=handle_client, args=(client_socket, client_address)).start()
