9ad1502d7cfb91cafcb7851801523e75ff5be6a8
alejandroestebangil <alejandroestebangil@gmail.com>
2024-03-10 15:53:35 +0100

    Limpieza básica del codigo

--- cliente.py
+++ cliente.py
@@ -5,13 +5,11 @@ import threading
 def receive_messages(client_socket):
     while True:
         try:
-            # Leer el mensaje del servidor
             message = client_socket.recv(1024).decode()
             if not message:
                 print("Conexión perdida con el servidor.")
                 break
             if message.lower().startswith("/help"):
-                # Si el mensaje es de ayuda, no lo imprimimos aquí
                 continue
             print(message)
         except ConnectionResetError:
@@ -25,27 +23,21 @@ def main():
     server_ip = "localhost"
     server_port = 9069
     client_name = input("Ingrese su nombre: ")
-
-    # Crear el socket del cliente
     client_socket = socket(AF_INET, SOCK_STREAM)
     
-    # Conectar al servidor
     try:
         client_socket.connect((server_ip, server_port))
     except Exception as e:
         print("Error al conectar al servidor:", e)
         sys.exit(1)
 
-    # Enviar el nombre del cliente al servidor
     client_socket.send(client_name.encode())
 
-    # Iniciar un hilo para recibir mensajes del servidor
     receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
     receive_thread.daemon = True
     receive_thread.start()
 
     while True:
-        # Leer el mensaje del usuario
         user_message = input(f"\n{client_name}: ")
         if user_message.lower().startswith("/msg"):
             parts = user_message.split(" ", 2)
@@ -61,23 +53,18 @@ def main():
             if len(parts) == 2:
                 new_name = parts[1]
                 user_message = f"/name {new_name}"
-                client_name = new_name  # Actualizar el nombre del cliente localmente
+                client_name = new_name
             else:
                 print("Comando mal formado. Uso: /name (nuevo_nombre)")
                 continue
         
-        # Enviar el mensaje al servidor
         client_socket.send(user_message.encode())
 
-        # Verificar si el usuario quiere salir del chat
         if user_message.lower() in ["/exit", "/quit"]:
             client_socket.close()
-            # Esperar a que el hilo de recepción termine antes de salir
             receive_thread.join()
             print("Has salido del chat.")
             break
-        
-        
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,33 +1,19 @@
 from socket import *
 import threading
 
-server_ip = "localhost"
-server_port = 9069
-
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind((server_ip, server_port))
-server_socket.listen(5)
-print("Server listening on port", server_port)
-
-clients = []
-
 def handle_list_command(client_socket, clients):
-    connected_clients = []
-    print("Total clients connected:", len(clients))
+    connected_clients = ["\nClientes conectados:"]
+    print("Total de clientes conectados: ", len(clients))
     for client in clients:
-        # Agrega el círculo verde antes del nombre del cliente
         connected_clients.append("\n\033[32m\u25CF\033[0m " + client[1])
-        # Imprime en el terminal del servidor
-        
         print("\033[32m\u25CF\033[0m", client[1])
     connected_clients_str = "".join(connected_clients)
-    #client_socket.send(connected_clients_str.encode())
-    return connected_clients
+    return connected_clients_str
 
-def handle_private_message(client_socket, sender_name, recipient_name, message):
+def handle_private_message(client_socket, sender_name, recipient_name, message, clients):
     for client in clients:
         if client[1] == recipient_name:
-            private_message = f"\n[Private from {sender_name}]: {message}"
+            private_message = f"\n[Mensaje privado de {sender_name}]: {message}"
             client[0].send(private_message.encode())
             return
     client_socket.send("El usuario especificado no está conectado o no es válido.".encode())
@@ -40,11 +26,9 @@ def handle_name_command(client_socket, clients, old_name, new_name):
     return "No se encontró el nombre de usuario especificado."
 
 def handle_client(client_socket, client_address, clients):
-    print(f"Accepted connection from {client_address}")
+    print(f"Conexion aceptada desde {client_address}")
     client_name = client_socket.recv(1024).decode()
     print(f"{client_name} se ha unido al chat")
-
-    # Agregar el nombre del cliente a la lista de clientes
     clients.append((client_socket, client_name))
 
     try:
@@ -52,7 +36,6 @@ def handle_client(client_socket, client_address, clients):
             message = client_socket.recv(1024).decode()
             if not message:
                 break
-            # El funcionamiento del /help
             if message.lower() == "/help":
                 help_message = (
                     "-/list : Te muestra la lista de personas conectadas al server\n"
@@ -72,13 +55,13 @@ def handle_client(client_socket, client_address, clients):
 
             if message.lower() == "/list":
                 response = handle_list_command(client_socket, clients)
-                client_socket.send("".join(response).encode())
+                client_socket.send(response.encode())
             if message.startswith("/name"):
                 parts = message.split(" ", 1)
                 if len(parts) == 2:
                     new_name = parts[1]
                     response = handle_name_command(client_socket, clients, client_name, new_name)
-                    client_name = new_name  # Actualizar el nombre del cliente en el servidor
+                    client_name = new_name
                     client_socket.send(response.encode())
                 else:
                     client_socket.send("Comando mal formado. Usa: /name (nuevo_nombre)".encode())
@@ -87,25 +70,35 @@ def handle_client(client_socket, client_address, clients):
                 if len(parts) == 3:
                     recipient_name = parts[1]
                     private_message = parts[2]
-                    handle_private_message(client_socket, client_name, recipient_name, private_message)
+                    handle_private_message(client_socket, client_name, recipient_name, private_message, clients)
                 else:
                     client_socket.send("Comando mal formado. Usa: /msg (usuario) (mensaje)".encode())
             else:
-                # Send the received message to all other clients
                 broadcast_message = f"\n{client_name}: {message}"
                 print(broadcast_message)
                 for client in clients:
-                    if client[0] != client_socket:  # Compare sockets
+                    if client[0] != client_socket:
                         client[0].send(broadcast_message.encode())
 
     except ConnectionResetError:
         print(f"La conexión con {client_name} ha sido reiniciada por el cliente.")
     
-    print(f"{client_name} has left the chat")
-    # Eliminar el cliente de la lista de clientes
+    print(f"{client_name} ha dejado el chat")
     clients.remove((client_socket, client_name))
     client_socket.close()
 
-while True:
-    client_socket, client_address = server_socket.accept()
-    threading.Thread(target=handle_client, args=(client_socket, client_address, clients)).start()
+def main():
+    server_ip = "localhost"
+    server_port = 9069
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(5)
+    print("Server usa el puerto", server_port)
+    clients = []
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        threading.Thread(target=handle_client, args=(client_socket, client_address, clients)).start()
+
+if __name__ == "__main__":
+    main()
